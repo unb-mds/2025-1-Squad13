7fa89406bf2d099dcfdaa23552e98b8934b50d4e
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-05-24 20:38:57 -0300

    Voice authentication script

--- voiceAltentication/audioCapture.py
+++ voiceAltentication/audioCapture.py
@@ -2,43 +2,63 @@ import pyaudio
 import wave
 import time
 import os
+import torchaudio
+from pathlib import Path
+from speechbrain.inference import SpeakerRecognition
 
-format = pyaudio.paInt16
-channel = 1
-rate = 44100
-recordingDuration = 5
-outputFile = "recording.wav"
+testingVoice = "testingVoice.wav"
+secretVoice = "secretVoice.wav"
 
-p = pyaudio.PyAudio()
+def recording(filename):
+    format = pyaudio.paInt16
+    channel = 1
+    rate = 44100
+    recordingDuration = 5
 
-stream = p.open(format=format, 
-                channels=channel,
-                rate = rate,
-                input=True,
-                frames_per_buffer=1024)
+    p = pyaudio.PyAudio()
 
-frames = []
+    stream = p.open(format=format, 
+                    channels=channel,
+                    rate = rate,
+                    input=True,
+                    frames_per_buffer=1024)
 
-os.system("clear")
+    frames = []
 
-print(f"Recording duration: {recordingDuration}s")
+    os.system("clear")
 
-aux = 0
-for i in range(0, int(rate / 1024 * recordingDuration)):
-    data = stream.read(1024)
-    frames.append(data)
-    if int(i % ((rate / 1024 * recordingDuration)/recordingDuration)) == 0:
-        print(f"{aux}s")
-        aux+=1
+    print(f"Recording duration: {recordingDuration}s")
+    if(filename == "testingVoice.wav"): print("Recording voice testing")
+    elif(filename == "secretVoice.wav"): print("Recording secret voice")
 
-print("Finish recording.")
+    aux = 0
+    for i in range(0, int(rate / 1024 * recordingDuration)):
+        data = stream.read(1024)
+        frames.append(data)
+        if int(i % ((rate / 1024 * recordingDuration)/recordingDuration)) == 0:
+            print(f"{aux}s")
+            aux+=1
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    print("Finish recording.")
 
-with wave.open(outputFile, 'wb') as wf:
-    wf.setnchannels(channel)
-    wf.setsampwidth(p.get_sample_size(format))
-    wf.setframerate(rate)
-    wf.writeframes(b''.join(frames))
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    with wave.open(filename, 'wb') as wf:
+        wf.setnchannels(channel)
+        wf.setsampwidth(p.get_sample_size(format))
+        wf.setframerate(rate)
+        wf.writeframes(b''.join(frames))
+
+arquivo = Path("secretVoice.wav")
+if(not arquivo.exists() and not arquivo.is_file()):
+    recording(secretVoice)
+
+recording(testingVoice)
+
+verification = SpeakerRecognition.from_hparams(
+                        source="speechbrain/spkrec-ecapa-voxceleb",
+                        savedir="pretrained_models/spkrec")
+
+score, prediction = verification.verify_files(testingVoice, secretVoice)
+print(f"Score: {score}, Match: {prediction}")
